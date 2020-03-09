65ba5e24c28117e4d69a9695b36ac90c62d10b76
sagarpansare <sagarprince2012@gmail.com>
2020-03-09 23:24:51 +0530

    first commit.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var createTable = () => {
+  let table = []
+
+  // Outer loop to create parent
+  for (let i = 0; i < 3; i++) {
+    let children = []
+    //Inner loop to create children
+    for (let j = 0; j < 5; j++) {
+      children.push(<td>{`Column ${j + 1}`}</td>)
+    }
+    //Create the parent and add the children
+    table.push(<tr>{children}</tr>)
+  }
+  return table
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        {createTable()}
+      </table>
     </div>
   );
 }
